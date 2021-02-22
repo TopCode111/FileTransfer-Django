dcf029dc2527017443b24982f267b7c3ad1d1d3a
TopCode111 <aldahir.martinez9197@gmail.com>
2021-02-22 14:17:41 +0800

    config ngix

--- techup/settings.py
+++ techup/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '7pgm!f062)+wellq8no=#kq2&=x+_^07i3cs0=qh1vd&s%$1tj'
+SECRET_KEY = '7pgm!f062)+wellq8no=#kq2&=x+_^07i3cs0=qh1vd&s%$123'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
