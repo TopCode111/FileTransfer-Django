d51f11b598b697827ef79e3060e0ca121e018363
TopCode111 <aldahir.martinez9197@gmail.com>
2021-03-14 22:19:13 +0800

    fixing encoding

--- techup/wsgi.py
+++ techup/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
+import urllib
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'techup.settings')
 
 application = get_wsgi_application()
+
+filename = urllib.parse.quote(filename) 
+response['Content-Disposition'] = 'attachment; filename="{}"'.format(filename)
